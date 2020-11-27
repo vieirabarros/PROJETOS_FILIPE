95aafcf6c284eb2ed8e992c65787f10808fec3a8
Filipe Barros <vieirabarros.filipe@gmail.com>
2020-11-27 19:40:06 -0300

    Update2

---
+++ criar-pagina/cards.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="row d-flex flex-row">
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+    </div>
+</div>
--- criar-pagina/index.php
+++ criar-pagina/index.php
@@ -8,20 +8,16 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
 
     <title>Hello, world!</title>
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
 
-    <div class="container">
-        <div></div>
-    </div>
-
-
-
-
+    <?php include 'navbar.php' ?>
+    <?php include 'carousel.php' ?>
+    <?php include 'cards.php' ?>
 
 
 
