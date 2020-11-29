2f8a7a28c19c02709f97cd4cf2e92c19319eb238
Filipe Barros <vieirabarros.filipe@gmail.com>
2020-11-29 12:40:08 -0300

    Update

---
+++ criar-pagina/comp-cards.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <div class="row d-flex flex-row">
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row d-flex flex-row">
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+        <div class="col-sm com-md d-flex flex-column">
+            <div class="card"> aafafafffa</div>
+        </div>
+    </div>
+</div>
