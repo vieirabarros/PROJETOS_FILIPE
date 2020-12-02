6e4d90a15de8442b060917c9463290fdbb468b65
Filipe Jonathan Vieira Barros <filipe.barros@CASACIVIL.LOCAL>
2020-12-02 16:37:12 -0300

    Update

---
+++ users-mgt-app/comp-descricaoPessoal.php
@@ -0,0 +1,32 @@
+<div class="container cc-descricaoPessoal">
+
+    <h2 class="titulo">Quem é quem</h2>
+
+    <div class="row">
+        <div class="col-sm col-md col-lg">
+            <div id="destaque1" class="descricaoPessoal d-flex">
+                <figure>
+                    <img src="http://via.placeholder.com/368x400" class="attachment-large size-large profile-img" alt="" loading="lazy">
+                    <figcaption>
+                        <ul>
+                            <li>
+                                <a href="mailto:flavia.salcedo@cge.ce.gov.br">
+                                    <img src="https://www.casacivil.ce.gov.br/wp-content/themes/ceara2017/assets/images/redesSociais/email.png" alt="E-mail">
+                                </a>
+                            </li>
+                        </ul>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+        <div class="col-sm col-md col-lg">
+            <div class="info">
+                <h4>Secretário-Chefe da Casa Civil</h4>
+                <h2>Flávio Jucá</h2>
+                <p><strong>Flávio Jucá é graduado em Administração de Empresas pela Universidade Estadual do Ceará (UECE) e Direito pela Universidade de Fortaleza (Unifor), com extensão em Administração Pública;pela Universidade Federal de Santa Catarina (UFSC).</strong></p>
+                <p>Possui especialização em Gerência de Marketing pela UECE/IEPRO. Foi empregado de carreira da;Caixa Econômica;Federal de 1982 até 2018, onde entre os vários cargos exercidos, foi;Gerente de Sustentação de;Negócios de Governo.</p>
+                <p>Cedido ao Governo do Estado do Ceará, exerceu de 2011 a 2014 o cargo de Coordenador de Habitação da Secretaria das Cidades,; foi assessor Especial no Gabinete do Governador e de 2015 a 2018 exerceu no Cargo de Secretário de Estado da Controladoria e Ouvidoria Geral, em 2019 assumiu o Cargo de Secretário Executivo de Gestão da SEPLAG.</p>
+                <p>Atualmente, também ocupa o cargo de secretario-chefe da Casa Civil do Governo do Estado.</p>
+            </div>
+        </div>
+    </div>
