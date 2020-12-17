ad0405cea613725dbb9f8f9912e1b71292d37328
vieirabarros <vieirabarros.filipe@gmail.com>
2020-12-17 08:21:41 -0300

    Update

--- Notas/jQuery.js
+++ Notas/jQuery.js
@@ -71,3 +71,96 @@ ready //occurs when the DOM has been loaded.
 resize //occurs when the browser window changes size.
 scroll //occurs when the user scrolls in the specified element.
 
+
+event object, which contains properties and methods related to the event:
+pageX, pageY the mouse position (X & Y coordinates) at the time the event occurred, relative to the top left of the page.
+type the type of the event (e.g. "click").
+which the button or key that was pressed.
+data any data that was passed in when the event was bound.
+target the DOM element that initiated the event.
+preventDefault() prevent the default action of the event (e.g., following a link).
+stopPropagation() Stop the event from bubbling up to other elements.
+
+
+Ex:.
+Shows which key was pressed
+
+$("div").keydown(function (event){
+alert(event.which)
+})
+
+Show where the mouse was at the time of the click
+
+$("element").click(function(event){
+alert(event.pageX)
+
+
+We can also trigger events programmatically using the trigger() method
+
+
+CREATE A TO DO LIST
+<h1>My To-Do List</h1>
+<input type="text" placeholder="New item" />
+<button id="add">Add</button>
+<ol id="mylist"></ol>
+
+$(function() {
+  $("#add").on("click", function() {
+      //event handler
+     //Create delete item button
+     var val = $("input").val();
+     if(val !== '') {
+       var elem = $("<li></li>").text(val);
+           $(elem).append("<button class='rem'>X</button>");
+                 $("#mylist").append(elem);
+                         $("input").val(""); //clear the input
+//Remove the parent
+$(".rem").on("click", function() {
+  $(this).parent().remove();
+  });
+                               }
+                                 
+        });
+        });
+
+
+
+
+Create a new div element and apprend it to the element with id test
+
+e=$("<div></div>")
+$("#test").append(e)
+
+
+hide()
+show()
+The toggle() method is used to toggle between hiding and showing elements.
+
+fadeIn(time)
+fadeOut(time)
+ fadeToggle(time)
+ fadeTo(time, opacity)
+
+
+
+slideUp() and slideDown() 
+methods are used to create a sliding effect on elements.
+Again, similar to the previous toggle methods, the 
+
+slideToggle()
+
+animate() method lets you animate to a set value, or to a value relative to the current value.
+
+
+$("div"). click(function(){
+$("div").animate({width:"100px"}, 1000);
+});
+
+$("element").animate({property:"value"}, time);
+
+Properties uses JSON camelCase format
+
+
+
+
+MODULAR DESIGN/ATOMIC
