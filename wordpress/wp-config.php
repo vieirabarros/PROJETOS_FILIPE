628270f4e9cd5e84f09acdfd0a422fd7dd5f6f06
Filipe Barros <vieirabarros.filipe@gmail.com>
2020-12-18 07:47:09 +0800

    Multisite

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -79,6 +79,15 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
+define('WP_ALLOW_MULTISITE', true);
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'localhost');
+define('PATH_CURRENT_SITE', '/PROJETOS_FILIPE/wordpress/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
